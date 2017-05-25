996662fea7575df27d89c68c34f46960b67b8ab5
Wayne W <wal_wang@yahoo.com>
2017-05-25 11:44:45 -0500

    fixed bucket name

--- cm/bootstrap.sh
+++ cm/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install httpd -y
 yum update -y
-aws s3 cp s3://ledavin-builds/jobs/AWS-Demo/latest.txt ./latest.txt
-aws s3 cp s3://ledavin-builds/jobs/AWS-Demo/`(cat latest.txt)`/artifacts/index.html /var/www/html/index.html
+aws s3 cp s3://cloudlp-demo/jobs/cloudlp-demo/latest.txt ./latest.txt
+aws s3 cp s3://cloudlp-demo/jobs/cloudlp-demo/`(cat latest.txt)`/artifacts/index.html /var/www/html/index.html
 service httpd start
 chkconfig httpd on
